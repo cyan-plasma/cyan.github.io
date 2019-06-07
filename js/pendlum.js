340674cdabbbcd111a0fc41d33033bdc81b35cbc
cyan-plasma <cyan@ssu.ac.kr>
2019-06-07 15:53:45 +0900

    .

--- js/pendlum.js
+++ js/pendlum.js
@@ -2,23 +2,24 @@
 
 angleMode = "radians";
 
-var angle = 0;
-var aVelocity = 0.0;
+var angle = 0.1;
+var aVelocity = 0.03;
 
 var draw = function() {
     background(255);
     
-    var amplitude = 100;
-    var x = amplitude * sin(angle);
+    var amplitude = 10;
+    var x = angle //amplitude * sin(angle);
     var y = amplitude * cos(angle);
 
-    angle += aVelocity;
-    
+//    angle += aVelocity;
+    angle += y
+
     resetMatrix();
     translate(width/2, height/2);
     
     stroke(0, 0, 0);
     fill(175, 175, 175);
-    line(0, 0, x, y);
+//    line(0, 0, x, y);
     ellipse(x, y, 20, 20);
 };
